0c26bb8f4bc05a3bfe19df9fce83da7ba5a9f22e
Joon9750 <junhyeok2166@daum.net>
2025-11-24 13:10:35 +0900

    add: ntt.h

--- src/kem/ntru_plus/KpqClean_ver2_NTRU_PLUS_KEM576_clean_montgomery-batch-noramlization/ntt.h
+++ src/kem/ntru_plus/KpqClean_ver2_NTRU_PLUS_KEM576_clean_montgomery-batch-noramlization/ntt.h
@@ -24,4 +24,13 @@ void basemul(int16_t r[4], const int16_t a[4], const int16_t b[4], int16_t zeta)
 #define basemul_add NTRUPLUS_NAMESPACE(basemul_add)
 void basemul_add(int16_t r[4], const int16_t a[4], const int16_t b[4], const int16_t c[4], int16_t zeta);
 
+#define fq_batchinv NTRUPLUS_NAMESPACE(fq_batchinv)
+void fq_batchinv(int16_t *r, const int16_t *a, int n);
+
+#define baseinv_calc_t_values NTRUPLUS_NAMESPACE(baseinv_calc_t_values)
+int baseinv_calc_t_values(int16_t t_values[3], int16_t *t3_out, const int16_t a[4], int16_t zeta);
+
+#define baseinv_apply_inv NTRUPLUS_NAMESPACE(baseinv_apply_inv)
+void baseinv_apply_inv(int16_t r[4], const int16_t a[4], const int16_t t_values[3], int16_t t3_inv);
+
 #endif
